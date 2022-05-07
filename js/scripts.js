4c3c7992ab12503bbb0291b46ddcba74ccdfc173
Sean Keane <seankeane93@gmail.com>
2022-05-06 18:02:15 -0700

    add Pizza function for business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 //Business Logic
 
+function Pizza {
+  constructor(size, toppings, crust) {
+    this.size = size,
+    this.toppings = toppings,
+    this.crust = crust
+  }
+
+Pizza.prototype.costCalculation = (pizzaArray, )
+}
