f47dac6b1da1e075980bd5695218c7a97e56f2c4
Sean Keane <seankeane93@gmail.com>
2022-05-06 18:35:48 -0700

    WIP creating else statements for costs associated with pizza traits

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,20 @@ const pizzaCrusts =  ['thin', 'cheese-stuffed', 'new-york', 'chicago'];
 const pizzaToppings = ['pepperoni', 'cheese','pineapple', 'bacon', 'mushrooms', 'peppers'];
 let startingPrice = 12;
 this.toppings.forEach(function(topping) {
-if(pizzaToppings.includes(topping)) {
-
+  if(pizzaToppings.includes(topping)) {
+    startingPrice += 3;
+  }
+})
+if(this.crust === "thin") {
+  startingPrice += 2;
+}else if(this.crust === "cheese-stuffed") {
+  startingPrice += 8;
+}else if(this.crust === "new-york") {
+  startingPrice  += 2;
+}else if(this.crust === "chicago") {
+  startingPrice += 7;
 }
-}
+if(this.size === 'medium') {
+  startingPrice += 5;
+}else if(this.size)
 }
